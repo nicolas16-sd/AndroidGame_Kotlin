e34ff01c825370f412d1a61ded77f2b79ec6f2f1
NICOLAS DOS SANTOS DURÃO <25132912@dsn127m1242375.cfp127aluno.local>
2026-03-02 09:46:45 -0300

    Término da Tela StartScreen, e início de desenvolvimento da Tela GameOverScreen

--- app/src/main/java/com/aulascom/exerciciowireframe/MainActivity.kt
+++ app/src/main/java/com/aulascom/exerciciowireframe/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
@@ -28,6 +29,7 @@ import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.aulascom.exerciciowireframe.ui.theme.ExercicioWireframeTheme
 
 class MainActivity : ComponentActivity() {
@@ -50,52 +52,89 @@ fun StartScreen(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
             .background(Color.Black)
-            .fillMaxSize()
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
 
-        Row(
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
+                .padding(top = 40.dp)
         ) {
-
-            Text(
-                text = "SCORE: 0050",
-                fontFamily = FontFamily.SansSerif,
-                fontWeight = FontWeight.Bold,
-                color = Color.White
-            )
-
             Row(
-                verticalAlignment = Alignment.CenterVertically
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(12.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
 
                 Text(
-                    text = "LIVES:",
+                    text = "SCORE: 0050",
                     fontFamily = FontFamily.SansSerif,
                     fontWeight = FontWeight.Bold,
                     color = Color.White
                 )
 
-                IconAndroid(modifier = Modifier.size(32.dp), color = Color.Green)
-                IconAndroid(modifier = Modifier.size(32.dp), color = Color.Green)
-                IconAndroid(modifier = Modifier.size(32.dp), color = Color.Green)
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+
+                    Text(
+                        text = "LIVES:",
+                        fontFamily = FontFamily.SansSerif,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    )
+
+                    IconAndroid(modifier = Modifier.size(15.dp), color = Color.Green)
+                    IconAndroid(modifier = Modifier.size(15.dp), color = Color.Green)
+                    IconAndroid(modifier = Modifier.size(15.dp), color = Color.Green)
+                }
             }
+
+            Row(modifier = Modifier
+                .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+
+            ){
+                IconAndroid(modifier = Modifier.size(75.dp), color = Color.Green)
+                IconAndroid(modifier = Modifier.size(75.dp), color = Color.Red)
+                IconAndroid(modifier = Modifier.size(75.dp), color = Color.Blue)
+                IconAndroid(modifier = Modifier.size(75.dp), color = Color.Yellow)
+                IconAndroid(modifier = Modifier.size(75.dp), color = Color.Green)
+            }
+
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .padding(bottom = 30.dp),
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            IconGame(modifier = Modifier.size(75.dp), color = Color(0, 128, 0, 255))
         }
 
         Row(modifier = Modifier
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .height(60.dp)
+            .padding(bottom = 24.dp)
+            .background(Color.Gray),
+            horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.CenterVertically
         ){
-            IconAndroid(modifier = Modifier.size(85.dp), color = Color.Green)
-            IconAndroid(modifier = Modifier.size(85.dp), color = Color.Red)
-            IconAndroid(modifier = Modifier.size(85.dp), color = Color.Blue)
-            IconAndroid(modifier = Modifier.size(85.dp), color = Color.Yellow)
-            IconAndroid(modifier = Modifier.size(85.dp), color = Color.Green)
+            Text(
+                text = "PRESS START",
+                fontSize = 24.sp,
+                fontFamily = FontFamily.SansSerif,
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
         }
-
     }
 }
 
@@ -103,8 +142,23 @@ fun StartScreen(modifier: Modifier = Modifier) {
 fun IconAndroid(modifier: Modifier = Modifier, color: Color) {
     Image(
         modifier = modifier,
-        painter = painterResource(R.drawable.ic_launcher_foreground),
+        painter = painterResource(R.drawable.ic_android_black_24dp),
         contentDescription = "Icon Android",
         colorFilter = ColorFilter.tint(color)
     )
+}
+
+@Composable
+fun IconGame(modifier: Modifier = Modifier, color: Color) {
+    Image(
+        modifier = modifier,
+        painter = painterResource(R.drawable.outline_activity_zone_24),
+        contentDescription = "Icon Game",
+        colorFilter = ColorFilter.tint(color)
+    )
+}
+
+@Composable
+fun GameOverScreen(modifier: Modifier = Modifier) {
+    
 }
